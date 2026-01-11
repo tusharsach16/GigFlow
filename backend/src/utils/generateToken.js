f7c23737e96bc6be8d47423d202ab74bdf84cbaf
Tushar <tusharschdeva16@gmail.com>
2026-01-11 23:29:19 +0530

    JWT token generation

---
+++ backend/src/utils/generateToken.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+const generateToken = (res, userId) => {
+    const token = jwt.sign({id: userId}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE || '1d'
+    });
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 24 * 60* 60* 1000
+    });
+
+    return token;
+};
+
+export default generateToken;
