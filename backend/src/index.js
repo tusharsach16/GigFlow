69731a8239f09364835bd0e7207da515b8418389
Tushar <tusharschdeva16@gmail.com>
2026-01-12 12:46:15 +0530

    integrate gig routes in index

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import authRoutes from './routes/auth.js';
+import gigRoutes from './routes/gig.js';
 
 dotenv.config();
 connectDB();
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/gigs', gigRoutes);
 
 const PORT = process.env.PORT || 5000;
 
