4f2bc62852808ddf0a6e19e465ab132c926ea6b4
Tushar <tusharschdeva16@gmail.com>
2026-01-14 02:00:19 +0530

    Remove redundant options handler

--- backend/src/index.js
+++ backend/src/index.js
@@ -59,7 +59,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
