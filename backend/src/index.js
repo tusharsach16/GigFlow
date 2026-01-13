b0661300fe6d2ea1339dd6e42430c49d86037672
Tushar <tusharschdeva16@gmail.com>
2026-01-14 00:00:51 +0530

    Created server for socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,26 +6,53 @@ import connectDB from './config/db.js';
 import authRoutes from './routes/auth.js';
 import gigRoutes from './routes/gig.js';
 import bidRoutes from './routes/bid.js';
+import {Server} from 'socket.io';
+import {createServer} from 'http';
 
 dotenv.config();
 connectDB();
 
 const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: process.env.CLIENT_URL || 'http://127.0.0.1:5173',
+        credentials: true
+    }
+})
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
     credentials: true
 }));
 
 app.use(express.json());
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/gigs', gigRoutes);
 app.use('/api/bids', bidRoutes);
 
+io.on('connection', (socket) => {
+    console.log('User connected: ', socket.id);
+    socket.on('join', (userId) => {
+        socket.join(userId);
+        console.log(`User ${userId} joined room`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected', socket.id);
+    });
+});
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
